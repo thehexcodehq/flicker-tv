a8e0524097032511515f8a4e02b38be7b1675dc9
Souvikarjun Deb <68196981+Souvikarjun@users.noreply.github.com>
2024-02-07 14:23:57 +0530

    quotes.js
    new quotes

--- quotes.js
+++ quotes.js
@@ -49,7 +49,26 @@ export default [
         author: "Jeff Bezos",
         submitter: "@hh-abir"
     },
-    
+        {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela",
+        submitter: "@souvikarjun"
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney",
+        submitter: "@souvikarjun"
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma, which is living with the results of other people's thinking.",
+        author: "Steve Jobs",
+        submitter: "@souvikarjun"
+    },
+    {
+        quote: "I am Groot",
+        author: "Groot",
+        submitter: "@souvikarjun"
+    },
     
 
 
